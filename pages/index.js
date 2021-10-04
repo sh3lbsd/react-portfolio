55735f940689ef5d3460fd0b273cf02e120c9d45
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-03 21:36:54 -0500

    added next link to index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <a href="/about"> About </a>
+        <Link href="/about"> About </Link>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
