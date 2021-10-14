6bb8db1341ab3d64631853d620660b57dafa5158
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-13 22:44:11 -0500

    adding player1/2 functionality

--- pages/about.js
+++ pages/about.js
@@ -1,31 +1,84 @@
-import styles from '../styles/about.module.css'
-
+import React from "react";
+import styles from "../styles/about.module.css";
 
 function Board() {
-return <div className={styles.board}>
-    <Square value={0}></Square>
-    <Square value={1}></Square>
-    <Square value={2}></Square>
-    <Square value={3}></Square>
-    <Square value={4}></Square>
-    <Square value={5}></Square>
-    <Square value={6}></Square>
-    <Square value={7}></Square>
-    <Square value={8}></Square>
+  const [player, setPlayer] = React.useState(1);
 
+  const [squareValue, setSquareValue] = React.useState();
+  const [squareValue1, setSquareValue1] = React.useState();
+  const [squareValue2, setSquareValue2] = React.useState();
+  const [squareValue3, setSquareValue3] = React.useState();
+  const [squareValue4, setSquareValue4] = React.useState();
+  const [squareValue5, setSquareValue5] = React.useState();
+  const [squareValue6, setSquareValue6] = React.useState();
+  const [squareValue7, setSquareValue7] = React.useState();
+  const [squareValue8, setSquareValue8] = React.useState();
+
+  return (
+    <div>
+      <h2>Your turn Player {player}!</h2>
+      <div
+        className={styles.board}
+        onClick={function () {
+          if (player === 1) {
+            setPlayer(2);
+          } else {
+            setPlayer(1);
+          }
+        }}
+      >
+        <Square
+          value={squareValue}
+          onClick={function () {
+            setSquareValue("X");
+          }}
+        ></Square>
+        <Square
+          value={squareValue1}
+          onClick={function () {
+            setSquareValue1("X");
+          }}
+        ></Square>
+        <Square
+          value={squareValue2}
+          onClick={function () {
+            setSquareValue2("X");
+          }}
+        ></Square>
+        <Square
+          value={squareValue3}
+          onClick={function () {
+            setSquareValue3("X");
+          }}
+        ></Square>
+        <Square
+          value={squareValue4}
+          onClick={function () {
+            setSquareValue4("X");
+          }}
+        ></Square>
+        <Square value={5}></Square>
+        <Square value={6}></Square>
+        <Square value={7}></Square>
+        <Square value={8}></Square>
+      </div>
     </div>
+  );
 }
 
-function Square({value}) {
-return <div className={styles.square}>{value}
-</div>
+function Square({ value, onClick }) {
+  return (
+    <div className={styles.square} onClick={onClick}>
+      {value}
+    </div>
+  );
 }
 
 export default function Home() {
-return <div className={styles.page}>
-    <h1>
-    Tic Tac Toe Exercise
-    </h1>
-    <Board></Board>
+  return (
+    <div className={styles.page}>
+      <h1>Tic Tac Toe Exercise</h1>
+      <Board></Board>
     </div>
+  );
 }
