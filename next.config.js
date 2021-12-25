4ce99e368b881247876f3607ea9396390f57e80d
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-12-24 18:37:46 -0600

    fix build error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
+      use: ["raw-loader", "glslify-loader"],
     });
     return config;
   },
-}
+};
